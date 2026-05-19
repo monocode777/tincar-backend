efcdc16bb4bef398c7dcb4449398208cc006dff1
Edgar Mojica <edgarmojik99@gmail.com>
2026-05-19 15:56:59 -0500

    fix mysql2

--- server.js
+++ server.js
@@ -82,7 +82,11 @@ const client =
 // ===============================
 // 🚀 SERVER
 // ===============================
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Servidor en puerto ${PORT}`);
+});
 db.connect((err) => {
 
   if (err) {

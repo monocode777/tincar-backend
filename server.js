34272b3a5dfb84e9d5d941d7f478c3e5e68d0d52
Edgar Mojica <edgarmojik99@gmail.com>
2026-05-20 18:09:01 -0500

    cloudinary working

--- server.js
+++ server.js
@@ -61,7 +61,18 @@ const storage =
 const upload = multer({
   storage: storage
 });
+cloudinary.config({
 
+  cloud_name:
+    process.env.CLOUDINARY_CLOUD_NAME,
+
+  api_key:
+    process.env.CLOUDINARY_API_KEY,
+
+  api_secret:
+    process.env.CLOUDINARY_API_SECRET,
+
+});
 // ===============================
 // 🛢️ MYSQL
 // ===============================
